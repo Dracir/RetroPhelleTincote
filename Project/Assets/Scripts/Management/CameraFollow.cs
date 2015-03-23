24cfbfeeafb5b5ae7cc576ab049aec95ab83cf64
mysteriosum <travis.h.martin@gmail.com>
2015-03-23 13:32:30 -0400

    adding platformer stuff and gym and placeholder arts
    Signed-off-by: mysteriosum <travis.h.martin@gmail.com>

---
+++ Project/Assets/Scripts/Management/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	//this script will follow the average point between all the objects in the array
+	public Transform[] toFollow;
+
+	//'init' should be used if we want to start zooming the camera in and out between the different points
+	//private Vector3 init;
+
+	private float lerpAmount = 0.1f;
+	public Vector3 offset = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		//init = transform.position;
+
+		if (toFollow == null){
+			GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+			toFollow = new Transform[players.Length];
+			int i = 0;
+			foreach (var item in players) {
+				toFollow[i] = item.transform;
+				i++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 averagePos = VectorFunctions.Midpoint(toFollow);
+
+		//take the average pos and lerp towards it; add the offset afterward
+		transform.position = Vector3.Lerp(transform.position, new Vector3(averagePos.x, averagePos.y, transform.position.z), lerpAmount) + offset;
+	}
+}
---
+++ Project/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Movable {
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+		base.Update();
+		controller.GetInputs();
+		
+		int hInput = 0;
+		if (controller.getR)
+			hInput ++;
+		if (controller.getL)
+			hInput --;
+		
+		Vel = Walk(hInput);
+		
+		if (controller.getJumpDown && grounded){
+			Vel = Jump();
+		}
+	}
+}
