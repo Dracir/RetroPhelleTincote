5d988c4b47cdb5b51f683b8b3d4793e6da684e1c
richard <rail.rich@gmail.com>
2015-03-24 14:17:06 -0400

    asd

--- Project/Maps/Tiles.tsx
+++ Project/Maps/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Tiles" tilewidth="64" tileheight="64">
- <image source="../Assets/Resources/Tilesets/Tileset.png" width="640" height="64"/>
+ <image source="../Assets/Resources/Tilesets/Tileset.png" width="640" height="320"/>
 </tileset>
