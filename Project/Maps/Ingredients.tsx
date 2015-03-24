de64690631985c8fdc9e704185ae2e56ce57c016
richard <rail.rich@gmail.com>
2015-03-24 12:49:25 -0400

    Super awesome Pusher

--- Project/Maps/Ingredients.tsx
+++ Project/Maps/Ingredients.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Ingredients" tilewidth="64" tileheight="64">
  <image source="../Assets/Resources/Tilesets/Ingredients.png" width="640" height="192"/>
+ <tile id="0">
+  <properties>
+   <property name="asd" value="asd"/>
+  </properties>
+ </tile>
 </tileset>
