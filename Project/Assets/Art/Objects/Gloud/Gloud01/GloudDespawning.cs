f6fb829de7446e7e3dc80d1a7207514c1bffa720
Magicolo <straty91@hotmail.com>
2015-03-26 11:25:34 -0400

    il ny a pas de speéeciaL

---
+++ Project/Assets/Art/Objects/Gloud/Gloud01/GloudDespawning.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Magicolo;
+
+public class GloudDespawning : State {
+	
+    Gloud Layer {
+    	get { return ((Gloud)layer); }
+    }
+	
+	public override void OnEnter() {
+		base.OnEnter();
+		
+	}
+	
+	public override void OnExit() {
+		base.OnExit();
+		
+	}
+}
