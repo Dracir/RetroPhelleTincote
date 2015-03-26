94977194a6658088c8d8a50c95f1be72ccc8cf44
mysteriosum <travis.h.martin@gmail.com>
2015-03-26 14:41:51 -0400

    sf

--- Project/Maps/Ingredients.tsx
+++ Project/Maps/Ingredients.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Ingredients" tilewidth="64" tileheight="64">
- <image source="../Assets/Resources/Tilesets/Ingredients.png" width="640" height="192"/>
+ <image source="../Assets/Resources/Tilesets/Ingredients.png" width="640" height="320"/>
 </tileset>
