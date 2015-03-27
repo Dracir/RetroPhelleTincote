6832ee1cc77c7213029054e385a9156bcefa4339
straty91 <straty91@hotmail.com>
2015-03-27 14:47:14 -0400

    background

---
+++ Project/Assets/Scripts/OscillateHue.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Magicolo;
+
+public class OscillateHue : MonoBehaviourExtended {
+
+	public float frequency = 0.5F;
+	public float amplitude = 360;
+	public float center = 180;
+	
+	bool _spriteRendererCached;
+	SpriteRenderer _spriteRenderer;
+	public SpriteRenderer spriteRenderer { 
+		get { 
+			_spriteRenderer = _spriteRendererCached ? _spriteRenderer : GetComponent<SpriteRenderer>();
+			_spriteRendererCached = true;
+			return _spriteRenderer;
+		}
+	}
+	
+	Material material {
+		get {
+			return spriteRenderer.material;
+		}
+	}
+	
+	void Update() {
+		Color hsv = material.color.ToHSV();
+		hsv.r = center + amplitude * Mathf.Sin(frequency * Time.time);
+		material.color = hsv.ToRGB();
+		Logger.Log(hsv.ToRGB(), hsv);
+	}
+}
+
